a649f476dbcc5a0134cde58d5691310b495bf3d3
kaizen <belebraz16@gmail.com>
2025-06-06 23:36:00 -0300

    wip

---
+++ src/app/domain/chat/chat.routes.ts
@@ -0,0 +1,26 @@
+import { Routes } from '@angular/router';
+import { TabsPage } from './tabs.page';
+
+export const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: '1',
+        loadComponent: () =>
+          import('../pages/chat.page').then((m) => m.ChatPage),
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/tab1',
+        pathMatch: 'full',
+      },
+    ],
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/tab1',
+    pathMatch: 'full',
+  },
+];
